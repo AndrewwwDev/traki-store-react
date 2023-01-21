1549ebe2bacf2bc43315e969c957aca8bc79a9dd
Andrew.Dev <andrewwwdev@gmail.com>
2023-01-21 00:12:59 -0400

    initial commit

---
+++ src/components/Footer/preguntas/PolíticasDePrivacidad.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+
+function PolíticasDePrivacidad() {
+  return (
+    <div className="jsx App">
+      <h1> POLÍTICAS DE PRIVACIDAD</h1>
+      <div
+        style={{ margin: "2rem auto 7rem", width: "85%" }}
+        className="footer__text"
+      >
+        <p>
+          <span style={{ fontWeight: "400" }}>
+            Esta Política de Privacidad está destinada a informar al Usuario
+            sobre el modo en la que se da tratamiento a su Información Personal.{" "}
+          </span>
+          <span style={{ fontWeight: "400" }}>LOREM IPSUM DOLOR </span>
+          <span style={{ fontWeight: "400" }}>
+            . se reserva el derecho de efectuar modificaciones a las presentes
+            políticas a los efectos de dar mejor tratamiento a la Información
+            Personal.{" "}
+          </span>
+        </p>
+        <p>
+          <span style={{ fontWeight: "400" }}>
+            Esta política no se aplica a los sitios a los que el Usuario pudiera
+            acceder por medio de links, o cualquier otro tipo de vínculo. En
+            caso de acceder a un sitio de un tercero por medio de un link
+            publicado en el Website, el Usuario deberá revisar detenidamente las
+            políticas de privacidad de dicho sitio.{" "}
+          </span>
+        </p>
+        <h3 style={{ margin: "20px 0 10px" }}>
+          <b>INFORMACIÓN PERSONAL</b>
+        </h3>
+        <p>
+          <span style={{ fontWeight: "400" }}>
+            El Website recopila la información que el Usuario ingresa referida a
+            su identidad a los efectos de operar en el mismo. Los datos que se
+            requieren a los Usuarios, son a los efectos de constatar no sólo la
+            identidad, sino también de acceder a ciertas y determinadas
+            funciones que hacen a la operatividad del Website. Todos los datos
+            así recabados, son estrictamente privados, y no estarán disponibles
+            de un modo público ni para conocimiento general.{" "}
+          </span>
+        </p>
+        <p>
+          <span style={{ fontWeight: "400" }}>
+            La Información Personal es recopilada y almacenada en los casos en
+            que ha sido brindada por el Usuario, o bien si ésta se ha obtenido
+            de fuentes de acceso público irrestricto.
+          </span>
+        </p>
+        <p>
+          <span style={{ fontWeight: "400" }}>
+            El usuario consiente que su Información Personal sea tratada de la
+            manera en la que se explica en este documento.
+          </span>
+        </p>
+        <h3 style={{ margin: "20px 0 10px" }}>
+          <b>USO DE LA INFORMACIÓN PERSONAL</b>
+        </h3>
+        <p>
+          <span style={{ fontWeight: "400" }}>LOREM IPSUM DOLOR </span>
+          <span style={{ fontWeight: "400" }}>
+            . no vende ni alquila a terceros Información Personal de sus
+            Usuarios.{" "}
+          </span>
+        </p>
+        <p>
+          <span style={{ fontWeight: "400" }}>
+            La utilización de dicha Información Personal es a exclusivos fines
+            propios, relacionados con el uso del Website, como ser el envío de
+            avisos o promociones que se difundan a través del mismo, o el envío
+            de nuestros newsletters o bien para mantener informado y actualizado
+            al Usuario sobre nuestras actividades, pero de ningún modo se
+            revelará esa Información Personal a terceros.
+          </span>
+        </p>
+        <h3 style={{ margin: "20px 0 10px" }}>
+          <b>POLÍTICA DE SEGURIDAD</b>
+        </h3>
+        <p>
+          <span style={{ fontWeight: "400" }}>
+            Proteger su privacidad es nuestro objetivo principal, por ello su
+            Información Personal está protegida por una contraseña. Hemos tomado
+            todas las medidas a nuestro alcance tecnológico y económico a fin de
+            asegurar tanto su privacidad como la nuestra. Toda la información
+            recabada se encuentra debidamente encriptada, salvo la que
+            eventualmente nos hubiera enviado vía email, de modo de asegurar que
+            las transacciones que se concierten tengan adecuado resguardo y
+            privacidad.
+          </span>
+        </p>
+        <p>
+          <span style={{ fontWeight: "400" }}>
+            En los casos de así estimarlo conveniente, y para brindar aún mayor
+            seguridad, podremos utilizar el protocolo de seguridad Secured
+            Socket Layer (SSL) para transferir datos del navegador al servidor.
+          </span>
+        </p>
+        <h3 style={{ margin: "20px 0 10px" }}>
+          <b>RÉGIMEN DE EXCEPCIONES A LA RESERVA DE LA INFORMACIÓN PERSONAL.</b>
+        </h3>
+        <p>
+          <span style={{ fontWeight: "400" }}>
+            La Información Personal y/o cualquier otra información personal del
+            Usuario a la que{" "}
+          </span>
+          <span style={{ fontWeight: "400" }}>LOREM IPSUM DOLOR </span>
+          <span style={{ fontWeight: "400" }}>
+            . hubiera accedido por cualquier medio, podrá ser revelada a
+            terceros, en cualquiera de los siguientes casos: (i) si hubiera sido
+            requerida por autoridad judicial o autoridad administrativa
+            competente; (ii) si hubiera necesidad de hacerlo en caso de
+            existencia de un intento de ruptura de la seguridad del Website.
+          </span>
+        </p>
+        <p>
+          <span style={{ fontWeight: "400" }}>
+            No asumimos ninguna responsabilidad por la difusión de Información
+            Personal que Ud. efectúe por cualquier medio distinto a los
+            previstos como de operación del Website.
+          </span>
+        </p>
+
+        <h3 style={{ margin: "20px 0 10px" }}>
+          <b>SEGURIDAD EN LA COMPRA</b>
+        </h3>
+        <p>
+          <span style={{ fontWeight: "400" }}>LOREM IPSUM DOLOR </span>
+          <span style={{ fontWeight: "400" }}>
+            {" "}
+            a través de su Website, garantiza la seguridad y confidencialidad en
+            todas sus comunicaciones con sus clientes. Todas las operaciones de
+            pago on-line se realizan a través de un servidor seguro, basado en
+            el estándar SSL y operados por empresas dedicadas exclusivamente a
+            dicha actividad, que protege los datos frente a intentos de
+            violación por terceros. Los datos del proceso de compra son
+            guardados por las empresas que intervienen en la operación, sin que
+            a este respecto{" "}
+          </span>
+          <span style={{ fontWeight: "400" }}>LOREM IPSUM DOLOR </span>
+          <span style={{ fontWeight: "400" }}>
+            . asuma responsabilidad alguna frente al Usuario.{" "}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default PolíticasDePrivacidad;
